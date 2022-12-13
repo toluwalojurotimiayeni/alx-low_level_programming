f690f179b9896bf570a685925849bfa7df719b76
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-13 06:36:22 -0800

    Times table commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,45 @@
 #include "main.h"
 
 /**
- * print_times_table - A function that prints the n times table
+ * print_times_table - A function that prints the n times table,
+ * starting with 0
  * @n: This is the number to be checked
  */
 
 void print_times_table(int n)
 {
-	int rows;
-	int columns;
-	int prod;
+	int rows, columns, prod;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (rows = 0; rows <= n; rows++)
 		{
-			for (columns = 0; columns <= 9; columns++)
+			_putchar('0');
+
+			for (columns = 0; columns <= n; columns++)
 			{
-				prod = (rows * columns);
-				if (columns == 0)
-					_putchar('0' + prod);
-				else
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+
+				prod = rows * columns;
+
+				if (prod <= 99)
 					_putchar(' ');
-					if (prod <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + prod);
-					}
-					else if (prod > 9 && prod < 100)
-					{
-						_putchar(',');
-						_putchar('0' + (prod / 10));
-						_putchar('0' + (prod % 10));
-					}
-					else if (prod >= 100)
-					{
-						_putchar('0' + (prod / 100));
-						_putchar('0' + ((prod / 10) % 10));
-						_putchar('0' + (prod % 10));
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
 	}
 }
+
