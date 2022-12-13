8eeda06960896d9008e18f12adce7071c5ab44de
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-13 08:15:53 -0800

    Fibonacci3 commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - A program that finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
