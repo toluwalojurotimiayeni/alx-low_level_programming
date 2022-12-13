6a3b79e52d84befc54031f79627aa5b142853028
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-13 07:35:03 -0800

    Fibonacci commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,13 @@
 int main(void)
 {
 	int count;
-	unsigned long n = 1;
-	unsigned long m = 2;
-	unsigned long sum = 50;
+	long n = 1;
+	long m = 2;
+	long sum = 50;
 
 	for (count = 1; count <= (sum / 2); count++)
 	{
 		printf("%li %li ", n, m);
-
 		n += m;
 		m += n;
 	}
