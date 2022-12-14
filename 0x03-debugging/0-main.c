0adb4d3907e11b6eb0e04d96fc7f81d382aaf6a7
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-14 04:40:18 -0800

    main commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - A test for a postitive_or_negative() function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+
+	return (0);
+}
