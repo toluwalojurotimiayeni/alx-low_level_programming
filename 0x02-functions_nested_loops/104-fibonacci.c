46e3dc6094a77ef9451f1fe939b90465b912ce01
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-13 17:36:29 -0800

    Fibonacci3 commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,17 @@
 
 int main(void)
 {
-	int count, number;
-	unsigned long n = 1;
-	unsigned long m = 1;
-	unsigned long sum = 0;
-	long n_half1, n_half2, m_half1, m_half2, sum_half1, sum_half2;
+	int count;
+	unsigned long n = 0, m = 1, sum;
+	unsigned long n_half1, n_half2, m_half1, m_half2, sum_half1, sum_half2;
 
-	printf("1");
-
-	for (count = 2; count < 93; count++)
+	for (count = 0; count < 92; count++)
 	{
 		sum = n + m;
+		printf("%lu, ", sum);
+
 		n = m;
 		m = sum;
-		printf(", %lu", sum);
 	}
 
 	n_half1 = n / 1000000000;
@@ -29,13 +26,19 @@ int main(void)
 	m_half1 = m / 1000000000;
 	m_half2 = m % 1000000000;
 
-	for (; count < 99; count++)
+	for (count = 93; count < 99; count++)
 	{
-		number = (n_half2 + m_half2) / 1000000000;
-		sum_half1 = (n_half2 + m_half2) - (1000000000 * number);
-		sum_half2 = (n_half1 + m_half1) + number;
+		sum_half1 = (n_half1 + m_half1);
+		sum_half2 = (n_half2 + m_half2);
+		if (n_half2 + m_half2 > 9999999999)
+		{
+			sum_half1 += 1;
+			sum_half2 %= 10000000000;
+		}
 
-		printf(", %lu%lu", sum_half1, sum_half2);
+		printf("%lu%lu", sum_half1, sum_half2);
+		if (count != 98)
+			printf(", ");
 
 		n_half1 = m_half1;
 		n_half2 = m_half2;
@@ -43,8 +46,6 @@ int main(void)
 		m_half2 = sum_half2;
 
 	}
-
 	printf("\n");
-
 	return (0);
 }
