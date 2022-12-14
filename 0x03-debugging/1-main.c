2549a60736643bfc43dc15284143b815af3134be
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-14 04:54:54 -0800

    Main2 commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Comment out the part of the code that is causing the output
+ *  to go into an infinite loop
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while (i < 10)*/
+	/* {*/
+	/* putchar(i);*/
+	/* }*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
