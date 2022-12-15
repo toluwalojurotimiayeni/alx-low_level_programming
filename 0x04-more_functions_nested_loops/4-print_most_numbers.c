20a595293dcef5ba4bd92a22fdef38fd0c9fd9ba
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-15 07:52:21 -0800

    Print most numbers commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_most_numbers - A function that prints the numbers,
  * from 0 to 9 skip 2 and 4
+ * Return: void
  */
 
 void print_most_numbers(void)
@@ -11,9 +12,9 @@ void print_most_numbers(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-		if (n != '2' && n != '4')
+		if (!(n == '2' || n == '4'))
 		{
-			_putchar(i);
+			_putchar(n);
 		}
 	}
 	_putchar('\n');
