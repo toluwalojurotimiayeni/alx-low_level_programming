bb96f283cbfb52eb611fd4c1183a747565f7b116
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-15 08:11:16 -0800

    Print numbers commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_numbers - A function that prints the numbers, from 0 to 9
- * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
-	char n;
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 0; n < 10; n++)
 	{
-		_putchar(n);
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }
