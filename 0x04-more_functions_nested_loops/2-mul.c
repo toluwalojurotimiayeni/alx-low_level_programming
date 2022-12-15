e90ae5ac36710cf097a54e6a6c19101f171e226c
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-15 07:28:47 -0800

    Multiple commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-A function that checks for a digit (0 through 9)
+A function that multiplies two integers
