b31cbc40a0491095a056b60fcd31d045a3ca77db
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-15 08:30:59 -0800

    Print line commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,9 +9,14 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar('-');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar(95);
+		_putchar('\n');
+	}
 }
