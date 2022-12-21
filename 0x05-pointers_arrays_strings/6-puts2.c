34bc4019a9a8193e68e99ff23d90da85ef3cfce2
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2022-12-21 06:53:06 -0800

    Puts2 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every other character of a string,
+ * starting with the first character
+ * @str: This is the string to be checked
+ * Return: Always 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+		if (n % 2 == 0)
+			_putchar(s[n]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-A function that reverses a string
+A function that prints every other character of a string, starting with the first character, followed by a new line
