658244e8ce1ad1427c3db09c91dcbd95d40f5b24
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2023-01-08 09:42:30 -0800

    Whats my name commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A program that prints its name, followed by a new line
+ * @argc: This counts the arguments
+ * @argv: This is the argument
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
