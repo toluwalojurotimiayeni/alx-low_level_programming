1ae8a43412933eb8ca62718df824d6198ebd9fa0
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2023-01-26 00:05:41 -0800

    Print list commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - A function that prints all the elements of a list_t list
+ * @h: This is sthe name of the list
+ * Return: The  number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int list = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		list++;
+		h = h->next;
+	}
+	return (list);
+}
