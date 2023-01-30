2b69adb596a6aa4467bad481cf4245dd7e89bc1a
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2023-01-30 10:03:08 -0800

    Pop listint  commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,13 +11,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *start;
-	int x;
+	int n;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 	start = *head;
 	*head = start->next;
-	x = start->x;
+	n = start->n;
 	free(start);
-	return (x);
+	return (n);
 }
