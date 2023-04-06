07504510e65585c40bae3dde8a8f81fa5cf43634
Toluwaloju Folashade Rotimi-Ayeni <profrotez@gmail.com>
2023-04-06 04:16:55 -0700

    Key index commit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,11 +8,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int idx;
-
-	if (size == 0)
-		return (0);
-
-	idx = hash_djb2(key);
-	return (idx % size);
+	return (hash_djb2(key) % size);
 }
